e8973edd43223d8608b6b02e5a3a7610051cf9be
Michael Mucciarone <mikemucc@gmail.com>
2020-01-25 12:21:46 -0600

    updating script and README

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 ## Usage
 
-Modify `deploy-screenlogic.sh` with the fqdn/ip address of the machine that the api is reachable on.
+This deploy script attempt to simplify the deployment of the ScreenLogic Frontend and API on your network. They only work on a linux machine, and will fail on a Mac or Windows Machine, as the docker net=host switch doesn't work properly on these platforms.
+
+### Deploy
+
+Modify `deploy-screenlogic.sh`. Specifically, edit the environment variable `SERVER_ADDRESS` to be the IP address or FQDN of the machine this will run on. Remember, this will become a website that you can access with a web browser.
+
+If you are trying to run on a Mac, you will need to figure out the IP address and Port that your ScreenLogic interface is running on. You can probably use your router to find it. Once you have that data, you can modify the `SL_IP_ADDRESS` and `SL_PORT` values in the script.
+
+### Access
+
+Once Deployed, you should be able to access it at `SERVER_ADDRESS` on port 9001 (9001 = P00l, get it?). This can be modified in the `docker-compose.yml` file if ports 9001 and 9002 are taken.
--- deploy-screenlogic.sh
+++ deploy-screenlogic.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 # Clean out any old containers
 docker-compose down
+# SERVER_ADDRESS should be the fqdn or ip address of the server these will be running on
+# The docker-compose file will run both containers on the same machine on different ports
+export SERVER_ADDRESS=""
 
 export API_IMAGE="michaelmucciarone/screenlogic-api:latest"
 export UI_IMAGE="michaelmucciarone/screenlogic-ui:latest"
-export SERVER_ADDRESS=""
+
 
 
 docker pull $API_IMAGE
 docker pull $UI_IMAGE
 
-export tempSL=$(docker run -it --rm --net=host michaelmucciarone/screenlogic-api:latest find-sl.js)
+# Attempt to find any local Screenlogics. This will only work on Linux, as --net=host doesn't work correctly on MacOS or Windows.
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    export tempSL=$(docker run -it --rm --net=host michaelmucciarone/screenlogic-api:latest find-sl.js)
+    echo Screenlogic found at $tempSL
+    export SL_IP_ADDRESS=$(echo $tempSL | cut -d ':' -f 1)
+    export SL_PORT=$(echo $tempSL | cut -d ':' -f 2)
+else
+    echo ScreenLogic not auto-detected on Network. Using specified configuration.
+    export SL_IP_ADDRESS="" # IP Address of Screenlogic.
+    export SL_PORT="80" # Port the ScreenLogic is running on. This is usually port 80
+fi
 
-echo Screenlogic found at $tempSL
-export SL_IP_ADDRESS=$(echo $tempSL | cut -d ':' -f 1)
-export SL_PORT=$(echo $tempSL | cut -d ':' -f 2)
 
 docker-compose up -d
