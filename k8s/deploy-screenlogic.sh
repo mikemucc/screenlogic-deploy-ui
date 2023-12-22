036a32ed578097fed420e4a4c8cceffc95b7793e
Michael Mucciarone <mikemucc@gmail.com>
2023-12-22 10:32:20 -0600

    write directly do the file...

--- k8s/deploy-screenlogic.sh
+++ k8s/deploy-screenlogic.sh
@@ -13,9 +13,8 @@ docker pull $API_IMAGE
 
 # Attempt to find any local Screenlogics. This will only work on Linux, as --net=host doesn't work correctly on MacOS or Windows.
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    export tempSL=$(docker run -it --rm --net=host michaelmucciarone/screenlogic-api:latest generate-configmap.js)
     echo Generating K8s ConfigMap
-    echo $tempSL > $CONFIGMAP_NAME
+    docker run -it --rm --net=host michaelmucciarone/screenlogic-api:latest generate-configmap.js > $CONFIGMAP_NAME
 else
     echo ERROR: This script only works on Linux. No configmaps created.
 fi
